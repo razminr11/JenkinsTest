5ac675a04d767554b14526a2317022418dad08e0
Raz <razminr11@gmail.com>
2015-02-27 15:38:47 -0800

    Test class written
    Wrote code for test class.

--- src/JenkinsTest.java
+++ src/JenkinsTest.java
@@ -1,7 +1,26 @@
+import java.awt.*;
+import java.io.*;
+import java.util.*;
+
 public class JenkinsTest
 {
+	public static Scanner uin = new Scanner(System.in);
+	
+	public static String input;
+	
 	public static void main(String[] args)
 	{
+		start();
+	}
+	
+	public static void start()
+	{
+		System.out.println("This is a test class.");
 		
+		System.out.println("Enter your name: ");
+		
+		input = uin.next();
+		
+		System.out.println("Hi "+uin);
 	}
 }
